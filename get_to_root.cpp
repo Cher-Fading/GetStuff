9efc49f03e80a0128b6d15543dde99b2e207342a
Xiaoning Wang <xw31@illinois.edu>
2020-07-19 22:48:53 -0400

    commit

--- get_to_root.cpp
+++ get_to_root.cpp
@@ -5,7 +5,7 @@ void get_to_root(const char *dataType = "")
 {
     std::ifstream file(Form("../GetStuff/%s.txt", dataType));
     std::string line;
-    std::ofstream outfile(Form("../GetStuff/%s_root.txt", dataType));
+    std::ofstream outfile(Form("../GetStuff/%s_root_pnfs.txt", dataType));
     int nl = 0;
     while (std::getline(file, line))
     {
