ae8362518abd73d81034395c068ab1e11b47afae
Xiaoning Wang <xw31@illinois.edu>
2020-08-05 14:26:33 -0400

    commit

---
+++ GetLOCALGROUPDISK_PbPb.sh
@@ -0,0 +1,6 @@
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420015.Pythia8EvtGen_JZ5R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets > ~/GetStuff/$2\.txt
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420014.Pythia8EvtGen_JZ4R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets >> ~/GetStuff/$2\.txt
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420013.Pythia8EvtGen_JZ3R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets >> ~/GetStuff/$2\.txt
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420012.Pythia8EvtGen_JZ2R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets >> ~/GetStuff/$2\.txt
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420011.Pythia8EvtGen_JZ1R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets >> ~/GetStuff/$2\.txt
+rucio list-file-replicas --expression BNL-OSG2_LOCALGROUPDISK user.xiaoning:user.xiaoning.420010.Pythia8EvtGen_JZ0R04.AOD.e4108_d1516_r11439_r11217..$1_Akt4HIJets >> ~/GetStuff/$2\.txt
