aa4f3fac8bdc88f5969159d09e9a36ccb632c14e
Cher-Fading <xw31@illinois.edu>
2020-08-07 18:17:49 -0500

    commit

---
+++ generate_rerun_list.sh
@@ -0,0 +1,9 @@
+if [ "$2" == "true" ]; then
+    SUFFIX=_pnfs
+fi
+for ptLim in 50 100; do
+    for trkLim in 0.5 1.5 2.0 4.0; do
+        ls /usatlas/scratch/cher97/%s_IPEval$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_$ptLim.00_$trkLim\ _done$SUFFIX.txt
+        root -q 'missing_rerun.cpp("WorkingDefaultpp",true,ptLim,trkLim)'
+    done
+done
