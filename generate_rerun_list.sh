ca44747c76cac66c5730568b4c1a523e5ea7dfa5
Cher-Fading <xw31@illinois.edu>
2020-08-07 18:27:57 -0500

    commit

--- generate_rerun_list.sh
+++ generate_rerun_list.sh
@@ -3,7 +3,7 @@ if [ "$2" == "true" ]; then
 fi
 for ptLim in 50 100; do
     for trkLim in 0.5 1.5 2.0 4.0; do
-        ls /usatlas/scratch/cher97/%s_IPEval$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_$ptLim.00_$trkLim\ _done$SUFFIX.txt
-        root -q 'missing_rerun.cpp("WorkingDefaultpp",'$2',ptLim,trkLim)'
+        ls /usatlas/scratch/cher97/$1_IPEval$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_$ptLim.00_$trkLim\ _done$SUFFIX.txt
+        root -q 'missing_rerun.cpp("'$1'",'$2',ptLim,trkLim)'
     done
 done
