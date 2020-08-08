697da615a41fd0af9d6f9b38a604d71eb5efed49
Cher-Fading <xw31@illinois.edu>
2020-08-07 22:01:16 -0500

    commit

--- generate_rerun_list.sh
+++ generate_rerun_list.sh
@@ -3,8 +3,8 @@ if [ "$2" == "true" ]; then
 fi
 for ptLim in 50 100; do
     for trkLim in 0.5 1.5 2.0 4.0; do
-        echo ~/GetStuff/$1_$ptLim.00_$trkLim\_done$SUFFIX.txt
-        #ls /usatlas/scratch/cher97/$1_IPEval$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_$ptLim.00_$trkLim\_done$SUFFIX.txt
-        #root -q 'missing_rerun.cpp("'$1'",'$2','$ptLim','$trkLim')'
+        #echo ~/GetStuff/$1_$ptLim.00_$trkLim\_done$SUFFIX.txt
+        ls /usatlas/scratch/cher97/$1_IPEval$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_$ptLim.00_$trkLim\_done$SUFFIX.txt
+        root -q 'missing_rerun.cpp("'$1'",'$2','$ptLim','$trkLim')'
     done
 done
