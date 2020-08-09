2b01016b8ae603546535825a25a757c6d64768f6
Xiaoning Wang <xw31@illinois.edu>
2020-08-09 18:34:53 -0400

    commit

--- generate_rerun_list.sh
+++ generate_rerun_list.sh
@@ -1,10 +1,10 @@
 if [ "$2" == "true" ]; then
     SUFFIX=_pnfs
 fi
-for ptLim in 50 100; do
-    for trkLim in 0.5 1.5 2.0 4.0; do
-	echo ~/GetStuff/$1_$ptLim.00_$trkLim\_done$3$SUFFIX.txt
-        ls /usatlas/scratch/cher97/$1_$3$SUFFIX/*_$ptLim.00_$trkLim.root > ~/GetStuff/$1_$ptLim.00_$trkLim\_done$3$SUFFIX.txt
-        root -q 'missing_rerun.cpp("'$1'",'$2','$ptLim','$trkLim')'
+for ptLim in 50; do
+    for trkLim in 0.5; do
+	echo ~/GetStuff/$1_done$3$SUFFIX.txt
+        ls /usatlas/scratch/cher97/$1_$3$SUFFIX/*_$ptLim.00_$trkLim.root >~/GetStuff/$1_done$3$SUFFIX.txt
+        root -q 'missing_rerun.cpp("'$1'",'$2','$ptLim','$trkLim',"'$3'")'
     done
 done
