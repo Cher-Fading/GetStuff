e47d56d6846a0a22c2520f34ffe59ce9888cfeaf
Cher-Fading <xw31@illinois.edu>
2020-09-11 21:17:52 -0500

    commit

--- CountJets.cpp
+++ CountJets.cpp
@@ -15,7 +15,6 @@ char indexF_str[nFlav][10] = {"Light", "B", "C"};
 const int nType = 2;
 char type_str[nType][10] = {"Failed", "Passed"};
 
-
 //stat for b and light are the same, ratio accounts for additional bad jets expected based on previous small samples.
 //stat for c are some ratio to b/light.
 void initBranches(TChain *fChain)
@@ -78,19 +77,19 @@ void CountJets(const char *trainname, const char *filename)
     }
 
     int JZ = -1;
-	int tag = -1;
-	int NUM = -1;
-	bool inclusive = false;
-	bool PbPb = false;
-	bool pnfs = false;
-	std::string dataType = "";
+    int tag = -1;
+    int NUM = -1;
+    bool inclusive = false;
+    bool PbPb = false;
+    bool pnfs = false;
+    std::string dataType = "";
 
-    bool parsed = parse_filename(filename,JZ,tag,NUM,inclusive,PbPb,pnfs,dataType);
-	if (!parsed){
-		cout << "parsing failed" << endl;
-		return;
-	}
-
+    bool parsed = parse_filename(filename, JZ, tag, NUM, inclusive, PbPb, pnfs, dataType);
+    if (!parsed)
+    {
+        cout << "parsing failed" << endl;
+        return;
+    }
 
     std::ofstream fstatout(Form("/atlasgpfs01/usatlas/data/cher97/%s%s_Counts/%s_%d_%d_%d_counts.txt", dataType.c_str(), Type[PbPb], trainname, JZ, tag, NUM));
 
@@ -103,7 +102,7 @@ void CountJets(const char *trainname, const char *filename)
     //Float_t mcwg;
     //Float_t Fcal;
     Int_t njets = 0;
-    std::vector<float> *jet_pt = 0;   //MV2
+    std::vector<float> *jet_pt = 0;       //MV2
     std::vector<float> *jet_eta = 0;      //MV2
     std::vector<int> *jet_LabDr_HadF = 0; //label
     //std::vector<float> *jet_nConst;     //cuts not used (nConst > 1)
@@ -155,54 +154,58 @@ void CountJets(const char *trainname, const char *filename)
     {
         for (int i = 0; i < nFlav; i++)
         {
-            if (o == indexF[i]){
+            if (o == indexF[i])
+            {
                 order[o] = i;
-break;}
+                break;
+            }
             else
                 order[o] = -1;
         }
     }
-/*for (int o = 0; o < nOrder + 1; o++){
+    /*for (int o = 0; o < nOrder + 1; o++){
 cout << order[o] << endl;
 }*/
 
     for (Long64_t jentry = 0; jentry < nentries; jentry++)
     {
-	//cout << jentry << endl;
+        //cout << jentry << endl;
         b_njets->GetEntry(jentry);
         b_jet_pt->GetEntry(jentry);
         b_jet_eta->GetEntry(jentry);
         b_jet_LabDr_HadF->GetEntry(jentry); //label
         b_jet_truthMatch->GetEntry(jentry); //cuts (truthMatch == 1)
         b_jet_aliveAfterOR->GetEntry(jentry);
-	//cout << njets << endl;
+        //cout << njets << endl;
         for (int j = 0; j < njets; j++)
         {
             int flav = jet_LabDr_HadF->at(j);
             if (flav != 0 && flav != 4 && flav != 5)
                 continue;
             int passed = jet_pt->at(j) > (ptLim * 1e3) && fabs(jet_eta->at(j)) <= 2.1 && jet_aliveAfterOR->at(j) == 1 && jet_truthMatch->at(j) == 1;
-if (order[flav]<0){
-cout << flav << endl;
-cout << order[flav] << endl;
-cout << passed << endl;
-cout << "pt (MeV): " << jet_pt->at(j) << "; eta: " << jet_eta->at(j) << "; No electron overlap: " << jet_aliveAfterOR->at(j) << "; truthMatch: " << jet_truthMatch->at(j) << endl;
-return;}
-n[order[flav]][passed]++;
+            if (order[flav] < 0)
+            {
+                cout << flav << endl;
+                cout << order[flav] << endl;
+                cout << passed << endl;
+                cout << "pt (MeV): " << jet_pt->at(j) << "; eta: " << jet_eta->at(j) << "; No electron overlap: " << jet_aliveAfterOR->at(j) << "; truthMatch: " << jet_truthMatch->at(j) << endl;
+                return;
+            }
+            n[order[flav]][passed]++;
         }
     }
 
     for (int f = 0; f < nFlav; f++)
     {
-int total = 0;
+        int total = 0;
         for (int i = 0; i < nType; i++)
         {
             cout << type_str[i] << " for " << indexF_str[f] << " jet is " << n[f][i] << endl;
             fstatout << indexF_str[f] << " " << type_str[i] << " " << n[f][i] << endl;
-total = total + n[f][i];
+            total = total + n[f][i];
         }
-cout << "Total for " << indexF_str[f] << " jet is " << total << endl;
-fstatout << indexF_str[f] << " Total " << total << endl;
+        cout << "Total for " << indexF_str[f] << " jet is " << total << endl;
+        fstatout << indexF_str[f] << " Total " << total << endl;
     }
 
     fstatout.close();
