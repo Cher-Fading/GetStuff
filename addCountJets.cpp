82c6174f7f953207ada5c2e4d0d582c8f8e4e2ad
Cher-Fading <xw31@illinois.edu>
2020-09-14 03:15:13 -0500

    commit

--- addCountJets.cpp
+++ addCountJets.cpp
@@ -52,7 +52,7 @@ void addCountJets(std::string trainname, std::string dataType, bool PbPb, bool p
         if (!filec)
         {
             missed++;
-            mfile << line.c_str() << endl;
+            mfile << Form("/atlasgpfs01/usatlas/data/cher97/%s%s_Counts%s/%s_%d_%d_%d_counts.txt", dataType.c_str(), Type[PbPb], suffix[pnfs], trainname.c_str(), JZ, tag, NUM) << endl;
         }
         n_types = 0;
         while (getline(filec, line2))
