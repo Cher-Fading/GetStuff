6fa9c6aefc10c04314b7ea253405baf98b4190a7
Cher-Fading <xw31@illinois.edu>
2020-09-15 18:00:11 -0500

    commit

---
+++ makeSTree_Condor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#input="/atlasgpfs01/usatlas/data/cher97/mc16_5TeV.txt"
+if [ "$4" == "true" ]; then
+	SUFFIX=_pnfs
+fi
+input="../GetStuff/$2_root$SUFFIX.txt"
+#input="mc16_5TeV_short.txt"
+
+mkdir -p /usatlas/scratch/cher97/$2$3_small$SUFFIX
+
+#indexline=$1
+linenumber=0
+while IFS= read -r line; do
+	if [ $1 -eq $linenumber ]; then
+		mkdir -p '/usatlas/scratch/cher97/tempin'$linenumber
+		xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$linenumber		
+		filename=/usatlas/scratch/cher97/tempin$linenumber/$(ls /usatlas/scratch/cher97/tempin$linenumber)
+		root -b -q -l 'makeStree.C("'$5'","'$filename'","/usatlas/scratch/cher97","'$linenumber'")'
+		sleep 2
+		rm -rf '/usatlas/scratch/cher97/tempin'$linenumber
+	fi
+	linenumber=$((linenumber + 1))
+done <"$input"
