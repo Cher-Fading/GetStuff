c13dac0abdaad93713b018eb0fae0b997b1788da
Cher-Fading <xw31@illinois.edu>
2020-09-16 01:06:19 -0500

    commit

--- count_miss.sh
+++ count_miss.sh
@@ -1,7 +1,7 @@
 if [ "$3" == "true" ]; then
     SUFFIX=_pnfs
 fi
-ls /atlasgpfs01/usatlas/data/cher97/$1$2_Counts$3/*_*_*.txt >../GetStuff/$1_countsdone$SUFFIX.txt
+ls /atlasgpfs01/usatlas/data/cher97/$1$2_Counts$SUFFIX/*.txt >../GetStuff/$1_countsdone$SUFFIX.txt
 
 if [ "$2" == "PbPb" ]; then
     PbPb = true
