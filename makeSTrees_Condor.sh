a611182c7ec2b50a053527caf3c8817219701f66
Cher-Fading <xw31@illinois.edu>
2020-09-19 04:32:00 -0500

    commit

--- makeSTrees_Condor.sh
+++ makeSTrees_Condor.sh
@@ -5,9 +5,9 @@ if [ "$4" == "true" ]; then
 fi
 
 if [ "$6" == "true" ]; then
-input="../GetStuff/$2$3_donesmall$SUFFIX.txt"
+	input="../GetStuff/$2$3_donesmall$SUFFIX.txt"
 else
-input="../GetStuff/$2$SUFFIX.txt"
+	input="../GetStuff/$2$SUFFIX.txt"
 fi
 #input="mc16_5TeV_short.txt"
 
@@ -18,16 +18,17 @@ linenumber=0
 while IFS= read -r line; do
 	if [ $1 -eq $linenumber ]; then
 		if [ "$6" == "true" ]; then
-filename=/usatlas/scratch/cher97/$2$3_small/$line
-else
-mkdir -p '/usatlas/scratch/cher97/tempin'$linenumber
-xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$linenumber
-filename=/usatlas/scratch/cher97/tempin$linenumber/$(ls /usatlas/scratch/cher97/tempin$linenumber)
-fi		
-
-		root -b -q -l 'makeSTree.C("'$5'","'$line'","'/usatlas/scratch/cher97/$2$3_smalls$SUFFIX'")'
+			filename=/usatlas/scratch/cher97/$2$3_small/$line
+		else
+			mkdir -p '/usatlas/scratch/cher97/tempin'$linenumber
+			xrdcp 'root://dcgftp.usatlas.bnl.gov:1096/'$line '/usatlas/scratch/cher97/tempin'$linenumber
+			filename=/usatlas/scratch/cher97/tempin$linenumber/$(ls /usatlas/scratch/cher97/tempin$linenumber)
+		fi
+		root -b -q -l 'makeSTree.C("'$5'","'$filename'","'/usatlas/scratch/cher97/$2$3_smalls$SUFFIX'")'
 		sleep 2
-		rm -rf '/usatlas/scratch/cher97/tempin'$linenumber
+		if [ "$6" == "true" ]; then
+			rm -rf '/usatlas/scratch/cher97/tempin'$linenumber
+		fi
 	fi
 	linenumber=$((linenumber + 1))
 done <"$input"
